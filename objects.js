80000ddbbefe70b840a8eb76528de15c250ce697
Victor Trofimciuc <victortrofimciuc@gmail.com>
2017-12-21 21:09:17 +0000

    automatically backed up by learn

---
+++ objects.js
@@ -0,0 +1,7 @@
+var meals = { breakfast: "oatmeal", lunch: "turkey sandwich", dinner: "steak and potatoes" };
+ 
+// the `delete` operator returns `true` if it has successfully
+// deleted, `false` otherwise
+delete meals.dinner; // true
+ 
+meals;
