6207c377830abb3a2051b7fabda15b2b28957272
Victor Trofimciuc <victortrofimciuc@gmail.com>
2017-12-21 21:13:10 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,8 +1,1 @@
-var meals = { breakfast: "oatmeal", lunch: "turkey sandwich", dinner: "steak and potatoes" };
- 
-// the `delete` operator returns `true` if it has successfully
-// deleted, `false` otherwise
-delete meals.dinner; // true
- 
-meals;
-console.log(meals);
+var playlist = Object.assign({})
